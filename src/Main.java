6f6c11a5a9c81b42cf3a5d7787c92cb39ec3e0f4
treskiiii <apontetq@mail.uc.edu>
2025-02-09 16:08:30 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+                // Predefined maintenance costs for each season
+                double springCost = 150.0;
+                double summerCost = 200.0;
+                double fallCost = 180.0;
+                double winterCost = 220.0;
+
+
+                double totalCost = springCost + summerCost + fallCost + winterCost;
+
+
+
+                System.out.println("Spring Maintenance Cost: $" + springCost);
+                System.out.println("Summer Maintenance Cost: $" + summerCost);
+                System.out.println("Fall Maintenance Cost: $" + fallCost);
+                System.out.println("Winter Maintenance Cost: $" + winterCost);
+
+                System.out.println("Total Yearly Maintenance Cost: $" + totalCost);
+            }
+        }
+
